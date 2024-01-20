c0c374bc29d8358752f132cf82a72259d85e8f69
eilonc-dev <120051919+eilonc-dev@users.noreply.github.com>
2024-01-20 23:01:52 +0200

    update os gathering

--- cmd/agent/main.go
+++ cmd/agent/main.go
@@ -1,7 +1,15 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+	"runtime"
+	"os"
+)
 
 func main() {
-    fmt.Println("Hello, world!")
+    fmt.Println("OS:", runtime.GOOS)
+    fmt.Println("Arch:", runtime.GOARCH)
+    fmt.Println("CPUs:", runtime.NumCPU())
+    fmt.Println("Go Version:", runtime.Version())
+    fmt.Println("Home Dir:", os.UserHomeDir())
 }
